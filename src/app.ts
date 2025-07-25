b1def4c887756757c35353ab6ca21a2bbc2c2183
Antonyj26 <antonyjosu26@gmail.com>
2025-07-25 16:59:27 -0300

    Creating error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { json } from "express";
+import { errorHandling } from "@/middlewares/errorHandling";
+import { route } from "@/routes/index";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(route);
+app.use(errorHandling);
 export { app };
